7628f2fabdf42d765a797cd12e08830d3b162265
juneheyoung <88702363+juneheyoung@users.noreply.github.com>
2023-02-06 11:56:43 +0900

    [Silver III] Title: N과 M (4), Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,18 @@
+N, M = list(map(int,input().split()))
+seq =[]
+def dfs():
+    if len(seq) == M:
+        print(' '.join(map(str,seq)))
+        return
+    for i in range(1,N+1):
+        if seq==[]:
+            seq.append(i)
+            dfs()
+            seq.pop(-1)
+        elif i>=seq[-1]:
+            seq.append(i)
+            dfs()
+            seq.pop(-1)
+            
+
+dfs()
