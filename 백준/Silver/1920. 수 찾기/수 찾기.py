3c3a18b27215e5ed5db8c89fc466b97463c781cb
juneheyoung <88702363+juneheyoung@users.noreply.github.com>
2023-02-18 17:18:02 +0900

    [Silver IV] Title: 수 찾기, Time: 452 ms, Memory: 46732 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,20 @@
+def find(x):
+    start=0
+    end = arr_count-1
+    while start<=end:                  # 2 2 3 4 5
+        mid = (start+end)//2           # 0 1 2 3 4     2 
+        if x == arr[mid]:
+            return 1
+        elif x <arr[mid]:
+            end = mid-1
+        else:
+            start = mid+1
+    return 0
+
+arr_count = int(input())
+arr = list(map(int,input().split()))
+arr.sort()
+target_count = int(input())
+target_list = list(map(int,input().split()))
+for i in target_list:
+    print(find(i))
